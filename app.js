9d452dc57eebeb43af63c4c1d39d4601064caa96
Vidyut Veedgav <vidyut.veedgav@gmail.com>
2023-12-01 21:27:38 -0500

    board created
    Signed-off-by: Vidyut Veedgav <vidyut.veedgav@gmail.com>

---
+++ app.js
@@ -0,0 +1,40 @@
+//defining global variables
+const gameBoard = document.querySelector("#gameboard"); //gameboard
+const playerDisplay = document.querySelector("#player"); //player
+const infoDisplay = document.querySelector("#info-display"); //info
+const width = 8; //width of gameboard
+
+const startPieces = [
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+     '', '', '', '', '', '', '', '', 
+     '', '', '', '', '', '', '', '', 
+     '', '', '', '', '', '', '', '', 
+     '', '', '', '', '', '', '', '', 
+     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+     rook, knight, bishop, queen, king, bishop, knight, rook,
+]
+
+function createBoard() {
+    startPieces.forEach((startPiece, i) => {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        square.innerHTML = startPiece
+        square.setAttribute('square-id', i)
+        //square.classList.add('beige')
+        const row = Math.floor((63 - i)/ 8) + 1
+        if (row % 2 === 0) {
+            square.classList.add(i % 2 === 0 ? "beige" : "brown");
+        } else {
+            square.classList.add(i % 2 === 0 ? "brown" : "beige");
+        }
+        if (i <= 15) {
+            square.firstChild.firstChild.classList.add('black');
+        } else if (i >= 48) {
+            square.firstChild.firstChild.classList.add('white');
+        }
+        gameBoard.append(square)
+    })
+}
+createBoard()
+ 
---
+++ pieces.js
@@ -0,0 +1,6 @@
+const king = '<div class="piece" id="king"><i class="fa-solid fa-chess-king"></i></div>'
+const queen = '<div class="piece" id="queen"><i class="fa-solid fa-chess-queen"></i></div>'
+const knight = '<div class="piece" id="knight"><i class="fa-solid fa-chess-knight"></i></div>'
+const bishop = '<div class="piece" id="bishop"><i class="fa-solid fa-chess-bishop"></i></div>'
+const rook = '<div class="piece" id="rook"><i class="fa-solid fa-chess-rook"></i></div>'
+const pawn = '<div class="piece" id="pawn"><i class="fa-solid fa-chess-pawn"></i></div>'
