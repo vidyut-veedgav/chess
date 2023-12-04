ad92ca02b6ac92ea2706690ed47bdb38fe0f2dcf
Vidyut Veedgav <vidyut.veedgav@gmail.com>
2023-12-04 02:18:45 -0500

    Game complete, must update UI

--- app.js
+++ app.js
@@ -634,12 +634,12 @@ function revertIds() {
 function checkForWin() {
     const kings = Array.from(document.querySelectorAll('#king'))
     console.log(kings);
-    if (kings.some(kings => king.firstChild.classList.contains('white'))) {
+    if (!kings.some(king => king.firstChild.classList.contains('white'))) {
         infoDisplay.innerHTML = "Black player wins!"
         const allSquares = document.querySelectorAll('.square')
         allSquares.forEach(square => square.firstChild?.setAttribute('draggable', false))
     }
-    if (kings.some(kings => king.firstChild.classList.contains('black'))) {
+    if (kings.some(king => king.firstChild.classList.contains('black'))) {
         infoDisplay.innerHTML = "White player wins!"
         const allSquares = document.querySelectorAll('.square')
         allSquares.forEach(square => square.firstChild?.setAttribute('draggable', false))
--- pieces.js
+++ pieces.js
@@ -1,6 +1,48 @@
+
+const king = '<div class="piece" id="king"><i class="fa-solid fa-shuttle-space"></i></div>'
+const queen = '<div class="piece" id="queen"><i class="fa-solid fa-jet-fighter-up"></i></div>'
+const knight = '<div class="piece" id="knight"><i class="fa-solid fa-helicopter"></i></div>'
+const bishop = '<div class="piece" id="bishop"><i class="fa-solid fa-motorcycle"></i></div>'
+const rook = '<div class="piece" id="rook"><i class="fa-solid fa-ship"></i></div>'
+const pawn = '<div class="piece" id="pawn"><i class="fa-solid fa-car"></i></div>'
+
+
+
+/*
+
 const king = '<div class="piece" id="king"><i class="fa-solid fa-chess-king"></i></div>'
 const queen = '<div class="piece" id="queen"><i class="fa-solid fa-chess-queen"></i></div>'
 const knight = '<div class="piece" id="knight"><i class="fa-solid fa-chess-knight"></i></div>'
 const bishop = '<div class="piece" id="bishop"><i class="fa-solid fa-chess-bishop"></i></div>'
 const rook = '<div class="piece" id="rook"><i class="fa-solid fa-chess-rook"></i></div>'
 const pawn = '<div class="piece" id="pawn"><i class="fa-solid fa-chess-pawn"></i></div>'
+
+
+//<i class="fa-solid fa-hippo"></i>
+//
+//
+
+/*
+
+pawn:
+<i class="fa-solid fa-cart-shopping"></i>
+
+rook:
+<i class="fa-solid fa-truck-front"></i>
+
+knight:
+<i class="fa-solid fa-horse"></i>
+
+bishop:
+<i class="fa-solid fa-motorcycle"></i>
+
+queen:
+<i class="fa-solid fa-jet-fighter-up"></i>
+
+king:
+<i class="fa-solid fa-sleigh"></i>
+
+
+
+
+*/
